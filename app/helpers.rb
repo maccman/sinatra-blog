ff2655600026141a2a409fe5db0ccd7d5381631d
Alex MacCaw <maccman@gmail.com>
2013-12-21 18:42:08 +0000

    improve styling

--- app/helpers.rb
+++ app/helpers.rb
@@ -1,4 +1,8 @@
 module Blog
   module Helpers
+    ::Date::DATE_FORMATS[:short_ordinal] = lambda { |date|
+      day_format = ActiveSupport::Inflector.ordinalize(date.day)
+      date.strftime("%b #{day_format}, %Y") # => "Dec 25th, 2007"
+    }
   end
 end
